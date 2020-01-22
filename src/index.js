07019790e234710249d0ccaf543fdde576869cc8
David Thomas <david.thomas@environment-agency.gov.uk>
2020-01-22 12:31:33 +0000

    Add bootstrap and pug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    res.render('home', { title: `David's recipes` })
+})
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'));
 
 const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+    console.log(`Express running → PORT ${server.address().port}`)
 });
 
