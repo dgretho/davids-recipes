500d091ca6d6146a5bfaa834e80e18a278b7a667
David Thomas <david.thomas@environment-agency.gov.uk>
2020-01-22 17:53:59 +0000

    Add a database and a .env file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 const path = require('path')
 
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+const Recipe = mongoose.model('recipe', new mongoose.Schema({
+    name: {
+        type: String
+    },
+    steps: {
+        type: [String]
+    },
+}))
+
 app.get('/', (req, res) => {
-    res.render('home', { title: `David's recipes` })
+    Recipe.find()
+        .then((recipes) => {
+            res.render('home', { title: `David's recipes`, recipes })
+        })
+        .catch(() => { res.send('Sorry! Something went wrong.'); })
 })
 
 app.set('views', path.join(__dirname, 'views'))
@@ -12,7 +38,7 @@ app.set('view engine', 'pug')
 
 app.use(express.static('public'));
 
-const server = app.listen(7000, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Express running → PORT ${server.address().port}`)
 });
 
